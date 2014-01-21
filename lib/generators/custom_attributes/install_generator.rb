89907257472b19a96596407ca8b5a99e5591b6b2
Christian Sommerauer <christian@picturk.com>
2014-01-21 21:51:07 +0000

    added entry table migration

--- lib/generators/custom_attributes/install_generator.rb
+++ lib/generators/custom_attributes/install_generator.rb
@@ -6,14 +6,10 @@ module CustomAttributes
     class InstallGenerator < Rails::Generators::Base
 
       desc "Move all the migrations to db/migrate"
-
       include Rails::Generators::Migration
       source_root File.expand_path("../templates", __FILE__)
-      #def self.source_root
-      #  @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-      #end
 
-      MIGRATIONS = [ ]
+      MIGRATIONS = ['create_entry_table']
 
       def create_migration_file
         MIGRATIONS.each do |migration_name|
@@ -27,19 +23,19 @@ module CustomAttributes
 
       protected
 
-      #def self.next_migration_number(dirname)
+      def self.next_migration_number(dirname)
         
-      #  if ActiveRecord::Base.timestamped_migrations
-      #    unless @prev_migration_nr
-      #      @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-      #    else
-      #      @prev_migration_nr += 1
-      #    end
-      #    @prev_migration_nr.to_s
-      #  else
-      #    "%.3d" % (current_migration_number(dirname) + 1)
-      #  end
-      #end
+        if ActiveRecord::Base.timestamped_migrations
+          unless @prev_migration_nr
+            @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+          else
+            @prev_migration_nr += 1
+          end
+          @prev_migration_nr.to_s
+        else
+          "%.3d" % (current_migration_number(dirname) + 1)
+        end
+      end
       
     end
   end
---
+++ lib/generators/custom_attributes/templates/create_entry_table.rb
@@ -0,0 +1,17 @@
+class CreateEntryTable < ActiveRecord::Migration
+   
+   def self.up
+		create_table :custom_attribute_entries, :force=>true do |t|
+			t.integer :custom_attributable_id
+			t.string :custom_attributable_type
+			t.integer :custom_value_id
+			t.string :custom_value_type
+			t.integer :custom_attribute_field_id
+			t.timestamps
+		end
+  end
+
+  def self.down
+    drop_table :custom_attribute_entries
+  end
+end
